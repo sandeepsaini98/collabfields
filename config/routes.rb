056265268346307e58d57be6c639ad40afd43fe5
vivek <aiethamvivek93@gmail.com>
2023-01-15 14:52:56 +0530

    implementing-bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#ind"
   root "pages#index"
+  #get "pages#ind"
+  #get '/pages', to: 'pages#index', as: 'pages'
 end
